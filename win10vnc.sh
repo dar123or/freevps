854e753c332ac095bf57a2826b9a271d74561006
dar123or <99406522+dar123or@users.noreply.github.com>
2022-02-12 11:49:03 +0700

    Update win10vnc.sh

--- win10vnc.sh
+++ win10vnc.sh
@@ -3,13 +3,13 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
-echo Please wait for installing...
+echo "Please wait for installing..."
 sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o Win10_21H2_English_x32.iso https://software-download.microsoft.com/pr/Win10_21H2_English_x32.iso?t=7c15efcf-dc3e-45b3-9a52-d94b8b4b5c5b&e=1644720342&h=c672b37972bb48ef8fdea1be640e34ad
-echo "Windows 10 On Google Colab"
+curl -L -o w7x86.img https://bit.ly/akuhnetw7x86
+echo "Windows 7 On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
@@ -17,3 +17,4 @@ echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by Aji
 sudo qemu-system-x86_64 -vnc :0 -hda Win10_21H2_English_x32.iso  -smp cores=4  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
 sleep 43200
+
